f5967474d958454f16a93b6fd39a5308ce4b5aae
Alan LaMielle <lamielle@netcitadel.com>
2013-07-15 10:07:07 -0700

    Extends reactive.dom with a few utilities
    Adds an identity function, bindPropertyTo, and bindToChecked to reactive.dom.

--- src/reactive.dom.js
+++ src/reactive.dom.js
@@ -2,6 +2,7 @@
   var identity = function (v) { return v };
 
   if (!$R) { return; }
+  $R.identity = function() {return $R(identity);};
   $R.dom = function(inputEl) {
     return new $R.Dom(inputEl);
   }
@@ -13,6 +14,13 @@
       formatter = formatter || identity
       $R(function(v) {
         if (this.el[0]) { this.el[0][attribute] = formatter(v) };
+      }, this).bindTo(rf);
+      return this;
+    },
+    bindPropertyTo: function (property, rf, formatter) {
+      formatter = formatter || identity
+      $R(function(v) {
+        this.el.prop(property, formatter(v));
       }, this).bindTo(rf);
       return this;
     },
@@ -34,6 +42,16 @@
       $(input).on("change", function () {
         rf(sanitizer(this.value));
       })
+      return this;
+    },
+    bindToChecked: function(input, sanitizer) {
+      sanitizer = sanitizer || identity;
+      var rf = this;
+      // Update to the initial state first, then update on every change.
+      rf(sanitizer($(input).is(':checked')));
+      $(input).change(function () {
+        rf(sanitizer($(this).is(':checked')));
+      });
       return this;
     }
   });
