4c0380022176c255d9b0a3f2e34e5c4c707064e2
Alan LaMielle <lamielle@netcitadel.com>
2013-07-30 08:05:19 -0700

    Adds a few more utilities to reactive.dom
    Warning: this is an API breaking change!  Renames bindToInput to bindToChange
    and adds a new bindToInput.  Each of these bind to the respective jQuery event
    of their namesake ('input' and 'change' respectively).  Also consistently forces
    an initial evaluation of the bound function so it begins in an initialized state
    (rather than only being called when the event triggers the first time).
    
    Also adds two new utilities:
    -bindPropertyToEach for binding to many a function's output to many
    elements in a single call.
    -bindToggleClassTo for binding a functions output to the toggleClass jQuery
    method so a class may be toggled (not just added).

--- src/reactive.dom.js
+++ src/reactive.dom.js
@@ -5,22 +5,37 @@
   $R.identity = function() {return $R(identity);};
   $R.dom = function(inputEl) {
     return new $R.Dom(inputEl);
-  }
+  };
   $R.Dom = function (inputEl) {
     this.el = $(inputEl);
-  }
+  };
   $R.Dom.prototype = {
     bindAttributeTo: function (attribute, rf, formatter) {
-      formatter = formatter || identity
+      formatter = formatter || identity;
       $R(function(v) {
-        if (this.el[0]) { this.el[0][attribute] = formatter(v) };
+        if (this.el[0]) { this.el[0][attribute] = formatter(v) }
       }, this).bindTo(rf);
       return this;
     },
     bindPropertyTo: function (property, rf, formatter) {
-      formatter = formatter || identity
+      formatter = formatter || identity;
       $R(function(v) {
         this.el.prop(property, formatter(v));
+      }, this).bindTo(rf);
+      return this;
+    },
+    bindPropertyToEach: function (property, rf, formatter) {
+      formatter = formatter || identity;
+      $R(function(v) {
+        this.el.each(function() {
+            $(this).prop(property, formatter(v));
+        });
+      }, this).bindTo(rf);
+        return this;
+    },
+    bindToggleClassTo: function (className, rf) {
+      $R(function(v) {
+        this.el.toggleClass(className, v);
       }, this).bindTo(rf);
       return this;
     },
@@ -33,15 +48,27 @@
       rf.bindToInput(this.el, sanitizer);
       return this;
     }
-  }
+  };
 
   $R.extend($R.pluginExtensions, {
+    bindToChange: function(input, sanitizer) {
+      sanitizer = sanitizer || identity;
+      var rf = this;
+      // Update to the initial state first, then update on every change.
+      rf(sanitizer($(input).val()));
+      $(input).on("change", function () {
+        rf(sanitizer(this.value));
+      });
+      return this;
+    },
     bindToInput: function(input, sanitizer) {
       sanitizer = sanitizer || identity;
       var rf = this;
-      $(input).on("change", function () {
+      // Update to the initial state first, then update on every change.
+      rf(sanitizer($(input).val()));
+      $(input).on("input", function () {
         rf(sanitizer(this.value));
-      })
+      });
       return this;
     },
     bindToChecked: function(input, sanitizer) {
